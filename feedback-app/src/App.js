2f260573aaac04140f2f6a786f3f56bace8ee1d3
huntersteffner <huntersteffner@gmail.com>
2022-12-09 19:13:42 -0500

    Props populate to feedback items

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,9 +1,18 @@
+import { useState } from 'react'
+
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from './data/FeedbackData'
+
 const App = () => {
-    
+  const [feedback,setFeedback] = useState(FeedbackData)
   return (
-    <div>
-        <h1>My App</h1>
-    </div>
+    <>
+      <Header  />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
   )
 }
 
