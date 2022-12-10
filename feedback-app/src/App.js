1f48168e26b28fdc39b7bfe205bdb58bed21d93e
huntersteffner <huntersteffner@gmail.com>
2022-12-10 10:51:50 -0500

    Added form

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 const App = () => {
   const [feedback,setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id) => {
     
@@ -16,6 +23,7 @@ const App = () => {
     <>
       <Header  />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
